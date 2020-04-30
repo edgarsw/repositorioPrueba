6d2c201b1fd474e3081a7755fb7b6f282dc4b95a
edgarsw <edgapin@hotmail.com>
2020-04-30 12:47:29 -0500

    Prueba uno

--- backend/database.js
+++
@@ -1,8 +0,0 @@
-const mongooose = require('mongoose')
-const URI = 'mongodb://localhost/crud'
-
-mongooose.connect(URI)
-.then(db => { console. log("DB is connected")})
-.catch(err => {console.log(`Error connection: ${err}`)})
-
-module.exports = mongooose
